5c464e137eab64609fac862b34e41a6a84a91d37
Felipe <felipesiqueira1212@gmail.com>
2019-09-30 11:30:13 -0300

    Aqui adicionamos os cards com os cursos e fotos e ajuste do header

--- index.php
+++ index.php
@@ -12,17 +12,51 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <header class= "d-flex justify-content-between align-items-center p-3">
+    <header class= "navbar">
         <h1 id="logo"><?php echo $nomeSistema;?></h1>
         <nav>
             <ul class="nav">
-                <li class="nav-item">Cursos</li>
-                <li class="nav-item">Login</li>
-                <li class="nav-item">Cadastrar</li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cursos</a>
+                    </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                    </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cadastrar</a>
+                    </li>
             </ul>
         </nav>
     </header>
-
+    <main>
+        <section class = "container mt-4">
+            <div class="row justify-content-between">
+                <div class="col-lg-3 card text-center">
+                    <h2>Curso de Admisnitração</h2>
+                    <img class="card-img-top" src="imagens/curso1.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">R$3.000,00</h5>
+                            <a href="#" class="btn btn-primary">COMPRAR</a>
+                        </div>
+                </div>
+                <div class="col-lg-3 card text-center">
+                    <h2>Curso de Engenharia</h2>
+                    <img class="card-img-top" src="imagens/curso2.jpeg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">R$5.000,00</h5>
+                            <a href="#" class="btn btn-primary">COMPRAR</a>
+                        </div>
+                </div>
+                <div class="col-lg-3 card text-center">
+                    <h2>Curso de Programação</h2>
+                    <img class="card-img-top" src="imagens/curso3.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">R$10.000,00</h5>
+                            <a href="#" class="btn btn-primary">COMPRAR</a>
+                        </div>
+                </div>
+        </section>
+    </main>
 
 
     </body>
